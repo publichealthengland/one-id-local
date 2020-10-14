a0c78401e7e1216442c03e22c903dfe0de512602
Farrukh Ayub <farrukh.ayub@phe.gov.uk>
2020-10-14 11:44:00 +0100

    init'

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	args := os.Args
+	if len(args) == 1 {
+		panic(errors.New("please app package names"))
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		js, err := json.Marshal(args[1:])
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(js)
+	}).Methods("GET")
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		panic(err)
+	}
+}
